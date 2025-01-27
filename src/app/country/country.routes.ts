43c8863a6ea90c3085af0bbfbaa9185686a3cf5e
Josue Eliezer Gomez Soto <jsegomez06@gmail.com>
2025-01-26 23:09:31 -0600

    footer and country pages added

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+
+export const countryRoutes: Routes = [
+    { 
+        path: '',         
+        loadComponent: () => import('./layouts/country-layout/country-layout.component'),
+        children: [
+            { path: 'by-capital', loadComponent: () => import('./pages/by-capital/by-capital.component') },
+            { path: '**', redirectTo: 'by-capital' }
+        ]
+    },
+];
+
+export default countryRoutes;
